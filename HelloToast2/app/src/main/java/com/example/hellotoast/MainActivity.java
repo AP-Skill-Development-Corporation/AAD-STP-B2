b573a3e5bc10231ad111a3ed27813ee95e6b3058
sai <saisankar75@gmail.com>
2020-12-30 14:06:09 +0530

    project updated

--- HelloToast2/app/src/main/java/com/example/hellotoast/MainActivity.java
+++ HelloToast2/app/src/main/java/com/example/hellotoast/MainActivity.java
@@ -1,22 +1,41 @@
 package com.example.hellotoast;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Create Variables
+    Button b1;
+    TextView tv;
+    int a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Configure View ID's
+        b1 = findViewById(R.id.countINC);
+        tv = findViewById(R.id.textcount);
+
+        //Handling Click Events
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // implement your own action
+                a++;
+                tv.setText(""+a);
+            }
+        });
     }
 
     public void displayToast(View view) {
         //Toast.makeText(context,"Text",Timeduration).show();
         Toast.makeText(this,"Hello APSSDC",Toast.LENGTH_LONG).show();
-
     }
 }
